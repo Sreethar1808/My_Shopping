79ce316ca89d483ded564553aaf6bf06c9cfbe18
sreetharraj <sreetharradha188@gmail.com>
2024-03-28 10:29:54 +0530

    Code setup

---
+++ src/test/java/com/My_Shopping/My_Shopping/MyShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.My_Shopping.My_Shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
