560f924e1b778c092556310319384d930e16f514
sreetharraj <sreetharradha188@gmail.com>
2024-04-03 21:06:21 +0530

    create the mail service

---
+++ src/main/java/com/My_Shopping/My_Shopping/service/MailService.java
@@ -0,0 +1,30 @@
+package com.My_Shopping.My_Shopping.service;
+
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    public void sendmail(String message, String buyermail, String subjectLine)
+    {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+        try{
+            mimeMessageHelper.setTo(buyermail);
+            mimeMessageHelper.setSubject(subjectLine);
+            mimeMessageHelper.setText(message);
+            javaMailSender.send(mimeMessage);
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+}
